e6d3f755387d2302394e4e0e3a11f0ce8dae5f90
Jakub Cieslar <cieslar.jakub@gmail.com>
2018-04-18 23:15:37 +0200

    config bundle path

--- lib/js/packer/rails/view_helpers.rb
+++ lib/js/packer/rails/view_helpers.rb
@@ -2,14 +2,9 @@ module Js
   module Packer
     module Rails
       module ViewHelpers
-        def js_test(name)
-          content_tag :script, 'abcdefg'
-        end
-
         def js_bundle_tag(*sources)
           sources.uniq.map do |source|
             bundle_name = js_bundle_name(source)
-            bundle_path = "/bundles/javascripts/#{bundle_name}"
             content_tag :script, '', src: "#{bundle_path}"
           end.join("\n").html_safe
         end
@@ -20,9 +15,35 @@ module Js
         end
 
         def js_bundle_name(source)
-          manifest_file = File.read("#{Rails.root}/public/bundles/javascripts/#{source}.json")
+          manifest_file = File.read(manifest_path(source))
           manifest = JSON.parse(manifest_file)
           bundle_name = manifest[source + '.js']
+        end
+
+        private
+
+        def manifest_path(source)
+          "#{::Rails.root}/#{config_manifest_path}/#{source}.json"
+        end
+
+        def bundle_path(bundle_name)
+          "/#{config_bundle_path}/#{bundle_name}"
+        end
+
+        def config?
+          File.exist?("#{::Rails.root}/config/packer.yaml")
+        end
+
+        def config
+          YAML.load_file("#{::Rails.root}/config/packer.yaml")[::Rails.env]
+        end
+
+        def config_manifest_path
+          config? ? config['manifest_path'] : 'public/bundles/javascripts'
+        end
+
+        def config_bundle_path
+          config? ? config['bundle_path'] : 'bundles/javascripts'
         end
       end
     end
